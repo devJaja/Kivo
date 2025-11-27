105a9d2fb97542b0a661ceeec23fb45816ee45a1
Dev Jaja <jajak5242@gmail.com>
2025-11-27 07:51:43 -0500

    fix: fix build error

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,9 +5,7 @@ const nextConfig: NextConfig = {
     images: {
       unoptimized: true,
     },
-    distDir: 'build', // optional, keeps .next folder clean
     basePath: '', // ensures links work as relative paths
-    assetPrefix: './',
   };
 
 export default nextConfig;
